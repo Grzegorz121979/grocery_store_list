92221d001d078135d4931831817f9967cf276dfa
Grzegorz121979 <pociechagrzegorz@outlook.com>
2024-05-28 20:13:11 +0200

    main function match

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 mod grocery;
 
 use std::error::Error;
+use std::io::{self, Write};
+
+use grocery::append_product_to_list;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = "shop_list.csv";
+    
+    loop {
+        
+        let file_path = "shop_list.csv";
+        let mut user_input: String = String::new();
+    
+        print!("Enter a - append product: ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut user_input)?;
+        let user_input: &str = user_input.as_str().trim();
+
+        match user_input {
+            "a" => {
+                match append_product_to_list(file_path) {
+                    Ok(()) => println!("Value append to the list"),
+                    Err(e) => eprintln!("{}", e),
+                }
+            },
+            _ => ()
+        }
+        break;
+    }
+
+    println!("Exit");
 
     Ok(())
 }
